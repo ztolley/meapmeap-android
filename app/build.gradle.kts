460e88e014616c39182ea88339a30db28f8984ba
Zac Tolley <zac@thetolleys.com>
2024-09-12 13:07:22 +0100

    Update build for Android SDK 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.exsite.meapmeap"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.exsite.meapmeap"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
